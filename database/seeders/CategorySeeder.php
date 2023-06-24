bf1aba2f5088b4f35070437effb3d51ec9b257a7
Umbeee <umberto.nesta@hotmail.com>
2023-06-24 15:28:02 +0200

    creata nuova tabella, con realtivo seeder 'categories'

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = [
+            'sport',
+            'animali',
+            'viaggi',
+            'giochi'
+        ];
+
+        foreach ($categories as $elem) {
+
+            $new_category = new Category();
+            $new_category->name = $elem;
+            $new_category->slug = Str::slug($new_category->name);
+            $new_category->save();
+        }
+    }
+}
